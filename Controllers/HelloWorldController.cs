d554abd498c5dfaf882e9576f6cfaab7b22a482a
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-06-30 00:37:43 +0300

    web app with ASP NET
    my ASP NET proejct

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel() { Name = "Sif", Age = 2 };
+            return View(doggo);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
