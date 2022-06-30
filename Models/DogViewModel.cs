e7ea6082bac25a2773baa1d6b46bf6c1f81ed26c
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-06-30 23:59:00 +0300

    my ASP NET application
    my ASP NET application

--- Models/DogViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FirstMVCApp.Models
-{
-    public class DogViewModel
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }    
-    }
-}
--- Pages/Checkout/Checkout.cshtml.cs
+++ Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizzeria.Pages.Checkout
 {
+    [BindProperties]
     public class CheckoutModel : PageModel
     {
+       
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (String.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
